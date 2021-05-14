9b4f81a2b6e4e09b877c60df0c6e0a974f4ebe67
Yunok <ahyz0569@gmail.com>
2021-05-14 18:01:04 +0900

    Map의 Key 타입 변경(String -> LocalDateTime)

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -5,6 +5,7 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
@@ -13,14 +14,13 @@ public class Crawler {
 
         /*
         상영시간표 정보를 담을 HashMap 생성
-        - Key: 상영날짜+상영시간(12자리, 숫자형태의 String 값)
+        - Key: 상영날짜+상영시간(12자리, 숫자형태의 String 값 -> LocalDateTime 타입으로 변경)
         - Value: 영화 제목
         */
-        HashMap<String, String> imaxScreenMoviesMap = new HashMap<>();
+        HashMap<LocalDateTime, String> imaxScreenMoviesMap = new HashMap<>();
 
         LocalDate now = LocalDate.now();
-        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyyMMdd");
-        String strDate = dateFormat.format(now);
+        String strDate = localDateToString(now);
 
         // 예매 페이지 URL
         String URL = "http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx" +
@@ -56,18 +56,20 @@ public class Crawler {
                         String playDate = timetableElement.attr("data-playymd");
                         String playTime = timetableElement.attr("data-playstarttime");
 
+                        LocalDateTime playDateTime = stringToLocalDateTime(playDate, playTime);
+
                         if (playTime.length() >= 1) {   // 상영 시작 시간은 뜨지만 예매가 마감된 경우, 시간표가 존재하지 않는 경우를 제외하기 위해서
 
                             /*
                             * 키로 등록할 값이 이미 존재하는 경우 while문 탈출
                             * ('sect-schedule'에 등록되지 않은 날짜를 조회하는 경우 예매가능한 가장 최근 날짜의 페이지로 redirect 되기 때문)
                             */
-                            if (imaxScreenMoviesMap.containsKey(playDate + playTime)) { 
+                            if (imaxScreenMoviesMap.containsKey(playDateTime)) {
                                 duplCheck = false;
                                 break;
                             } else {
 //                                System.out.println(playDate + playTime + " :: " + movieTitle);
-                                imaxScreenMoviesMap.put(playDate + playTime, movieTitle);
+                                imaxScreenMoviesMap.put(playDateTime, movieTitle);
                             }
                         }
                     }
@@ -75,7 +77,7 @@ public class Crawler {
                 }// for문
                 // 조회할 날짜 데이터 증가
                 LocalDate date = now.plusDays(i);
-                strDate = dateFormat.format(date);
+                strDate = localDateToString(date);
                 i++;
 
             } catch (IOException e) {
@@ -85,8 +87,33 @@ public class Crawler {
         }// while
 
         // HashMap 출력하기
-        for (String key : imaxScreenMoviesMap.keySet()) {
+        for (LocalDateTime key : imaxScreenMoviesMap.keySet()) {
             System.out.println(String.format("키 : %s, 값: %s", key, imaxScreenMoviesMap.get(key)));
         }
     }
+
+    // LocalDate -> String 타입으로 변환 (예: 2021-05-14 -> 20210514)
+    private static String localDateToString(LocalDate date) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        return date.format(dateFormatter);
+    }
+
+    // String(날짜, 시간) -> LocalDateTime 타입으로 변환 (예: 20210514, 2230 -> 2021-05-14T22:30)
+    private static LocalDateTime stringToLocalDateTime(String date, String time) {
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(4, 6));
+        int day = Integer.parseInt(date.substring(6, 8));
+
+        int hour = Integer.parseInt(time.substring(0, 2));
+        int minute = Integer.parseInt(time.substring(2, 4));
+
+        // 새벽타임 (예: 26:30 ) 예외처리 (다음날 새벽 시간으로 변환)
+        if (hour >= 24) {
+            hour -= 24;
+            day += 1;
+        }
+
+        return LocalDateTime.of(year, month, day, hour, minute);
+    }
+
 }
